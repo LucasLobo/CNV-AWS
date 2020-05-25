a6b126b2dbc2bf09d608d20286a91fa82bc93ff5
LucasLobo <lucas5798fell@gmail.com>
2020-05-25 21:39:59 +0100

    Updates Estimators periodically

--- codebase/LBInstance/project/pt/ulisboa/tecnico/cnv/server/WebServer.java
+++ codebase/LBInstance/project/pt/ulisboa/tecnico/cnv/server/WebServer.java
@@ -86,6 +86,7 @@ public class WebServer {
 	static EstimatorDLX estimatorDLX = new EstimatorDLX();
 	static EstimatorCP estimatorCP = new EstimatorCP();
 	static AtomicLong requestIds = new AtomicLong();
+	static AtomicLong lastSavedRequestId = new AtomicLong();
 
 	static HashMap<Long, Integer> requestCostEstimation = new HashMap<>();
 	static HashMap<Long, Integer> requestMethodProgress = new HashMap<>(); // needs to be converted to cost
@@ -97,6 +98,7 @@ public class WebServer {
 		startAutoScaler();
 		createMSS();
 		init();
+		updateEstimators();
 
 		final HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
@@ -116,7 +118,7 @@ public class WebServer {
 			public void run() {
 			  AutoScaler.startAS();
 			}
-		  }).start();
+		}).start();
 	}
 
 	private static void createMSS() {
@@ -214,7 +216,7 @@ public class WebServer {
 		return estimator.estimate(size, un);
 	}
 
-	private static List<Map<String,AttributeValue>> fetchEntriesHigherThan(int id) {
+	private static List<Map<String,AttributeValue>> fetchEntriesHigherThan(Long id) {
 		 Map<String, AttributeValue> expressionAttributeValues = new HashMap<String, AttributeValue>();
 		 expressionAttributeValues.put(":val", new AttributeValue().withN(String.valueOf(id)));
 		 ScanRequest scanRequest = new ScanRequest().withTableName(tableName).withFilterExpression("request_id > :val").withExpressionAttributeValues(expressionAttributeValues);
@@ -234,6 +236,49 @@ public class WebServer {
 			return -1;
 		}
 		return estimator.transform(methods);
+	}
+
+	private static void addPoint(String solver, Integer size, Integer un, Integer methods) {
+		Estimator estimator;
+		if (solver.equals("BFS")) {
+			estimator = estimatorBFS;
+		} else if (solver.equals("DLX")) {
+			estimator = estimatorDLX;
+		} else if (solver.equals("CP")) {
+			estimator = estimatorCP;
+		} else {
+			return;
+		}
+		estimator.addDataPoint(size, un, methods);
+	}
+
+	private static void updateEstimators() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (true) {
+						Long lastSaved = lastSavedRequestId.get();
+						Long biggestRequestId = 0L;
+						List<Map<String,AttributeValue>> mss_entries = fetchEntriesHigherThan(lastSaved);
+	
+						for (Map<String,AttributeValue> line : mss_entries) {
+							Long requestId = Long.parseLong(line.get("request_id").getN());
+							String solver = line.get("strategy").getS();
+							Integer size = Integer.parseInt(line.get("size").getN());
+							Integer un = Integer.parseInt(line.get("un").getN());
+							Integer methods = Integer.parseInt(line.get("cost").getN());
+							addPoint(solver, size, un, methods);
+							if (requestId > biggestRequestId) biggestRequestId = requestId;
+						}
+						lastSavedRequestId.set(biggestRequestId);
+						Thread.sleep(30*1000);
+					}
+				} catch (RuntimeException e) {
+				} catch (Exception e) {
+				}
+			}
+		}).start();	
 	}
 
 	static class ProgressChecksHandler implements HttpHandler {
